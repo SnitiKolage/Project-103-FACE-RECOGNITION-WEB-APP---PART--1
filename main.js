3bdeb624a101bd8f1e1f007a7a50d9a32c251bf4
SnitiKolage <78856504+SnitiKolage@users.noreply.github.com>
2021-07-10 12:47:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+Webcam.attach('#camera');
+camera = document.getElementById("camera");
+
+Webcam.set({
+    width: 350,
+    height:300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="' + data_uri + '"/>';
+
+    });
+
+}
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json', modelLoaded);
+
+function check() {
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3)
+    }
+}
+
